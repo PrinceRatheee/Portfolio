4867300b2662b225792f0f74edafd70a55870a1a
PrinceRatheee <rathi.prince2@gmail.com>
2024-01-26 11:41:34 +0530

    project completed

--- src/components/parallex/Parallax.jsx
+++ src/components/parallex/Parallax.jsx
@@ -27,7 +27,7 @@ const Parallax = ({ type }) => {
         {type === "services" ? "What I Do?" : "What I Did?"}
       </motion.h1>
       <motion.div  className="mountains"></motion.div>
-      <motion.div style={{y:yBg, backgroundImage:`url(${type==="services"?"/planets.png":"/sun.png"})`}} className="planets"></motion.div>
+      <motion.div style={{y:yBg, backgroundImage:`url(${type==="services"?"./planets.png":"./sun.png"})`}} className="planets"></motion.div>
       <motion.div style={{x:yBg}} className="stars"></motion.div>
     </div>
   );
